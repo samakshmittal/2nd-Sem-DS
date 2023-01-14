977ed998980f51a5b69d04d614a34e3c0c3bde15
Samaksh Mittal <114502092+samakshmittal@users.noreply.github.com>
2023-01-14 11:13:57 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int a[10];
+    for(int i=0; i<10; i++){
+            printf("Enter the value of a[%d]=",i);
+            scanf("%d", &a[i]);
+    }    
+    printf("The value of array is ");
+    for(int i=0; i<10; i++){
+        if(i==9){
+                printf("%d", a[i]);
+                break;
+        }
+        printf("%d, ", a[i]);
+    }
+    
+    return 0;
+}
