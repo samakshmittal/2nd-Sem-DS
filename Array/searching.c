5da1f27f0ee34c6f9328fbdb05aa3897d8efe878
samakshmittal <samakshmittal826@gmail.com>
2023-01-20 21:13:57 +0530

    Update searching.c

--- Array/searching.c
+++ Array/searching.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(){
-    char A[9]={'B','C','D','E','G','H','I','J', 'K'}, item='n';
-    int ub=8, lb=0, z=0;
+    char A[9]={'B','C','D','E','G','H','I','J', 'K'}, item='D';
+    int ub=8, i, z=0;
     for (int j=0; j<9; j++){
         printf("%c ", A[j]);
     }
     printf("\n");
-    for(int i=0; i<=ub; i++){
+    for(i=0; i<=ub; i++){
         if(A[i]==item){
-            printf("Successful");
+            printf("Successful at location %d", i);
             z=1;
             break;
         }
