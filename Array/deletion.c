0821627e7b6527ab13f48ce774efa7cef1a836a3
samakshmittal <samakshmittal826@gmail.com>
2023-01-20 21:14:01 +0530

    Update deletion.c

--- Array/deletion.c
+++ Array/deletion.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 int main(){
-    char A[100], ch;
+    int A[100], ch;
     int ub, j, n, loc, z=0;
     printf("Enter size of array");
     scanf("%d", &n);                               //Enter size of array
     printf("Enter elements in array\n");
     for (j=0; j<n; j++){                              //Entering elements in an array
         printf("Enter element A[%d]", j);
-        scanf("%c ", &A[j]);
+        scanf("%d ", &A[j]);
     }
     printf("\n");
     printf("Before deletion : ");
     for (j=0; j<n; j++){                              //Printing elements in an array before insertion
-        printf("%c ", A[j]);
+        printf("%d ", A[j]);
     }
     printf("\n");
     printf("Enter location from where element is to be deleted");        //Enter location from where element is to be deleted
-    scanf("%d", &loc);                    //Enter location of new element
+    scanf("%d", &loc);                    //Enter location of element to be deleted
     ub=n-1;
     int temp=loc, i=ub;
     while(z==0)
     {
         if(loc<n)
         {
-            while(i>=temp){                          //Making space for element
+            while(i>=temp){                          //Removing space
                 A[temp]=A[temp+1];
                 temp++;
             }
             n--;
             printf("\nAfter deletion : ");
-            A[loc-1]=ch;                             //Inserting element
-            for (int j=0; j<n; j++){                 //Printing array after insertion
-                printf("%c ", A[j]);
+            for (int j=0; j<n; j++){                 //Printing array after deletion
+                printf("%d ", A[j]);
             }
             z=1;
         }
