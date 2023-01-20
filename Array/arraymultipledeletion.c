e4f1c66670579a8aefdaf2aaaa197b5947064be7
samakshmittal <samakshmittal826@gmail.com>
2023-01-20 21:13:50 +0530

    Create arraymultipledeletion.c

---
+++ Array/arraymultipledeletion.c
@@ -0,0 +1,41 @@
+//Deletion of multiple elements in array
+#include <stdio.h>
+int main(){
+    int i, j, n, no, loc, z=0;
+    printf("Enter size of array");
+    scanf("%d", &n);                               //Enter size of array
+    int a[n];
+    printf("Enter elements in array\n");                    //Enter elements in array
+    for(i=0; i<n; i++){
+            printf("Enter the value of a[%d]=",i);
+            scanf("%d", &a[i]);
+    }
+    printf("\n");
+    printf("Array is : ");
+    for(i=0; i<n; i++){                             //Print elements of array
+            printf("Value of a[%d]=%d\n",i, a[i]);
+    }
+    printf("Enter number of elements to be deleted");
+    scanf("%d", &no);                                                  //Enter number of elements to be deleted
+    
+    for(j=1; j<=no; j++){
+        printf("Enter location of element to be deleted");
+        scanf("%d", &loc);
+        if(loc<n){                                         //Elements searching
+        while((n-1)>=loc){                          //Making space for element
+                a[loc]=a[loc+1];
+                loc++;
+            }
+            n--;
+            printf("\nAfter deletion : "); 
+            for (int j=0; j<n; j++){                 //Printing array after deletion
+                printf("%d ", a[j]);
+            }
+            printf("\n");
+    }
+    else{
+        printf("Invalid location\n");
+    }
+    }
+}
+//Repeat input of location after invalid input
