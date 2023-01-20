02bc7b9958dc19a573115b5842479ee35f73063a
samakshmittal <samakshmittal826@gmail.com>
2023-01-20 21:47:11 +0530

    Create arrayoddevensearch.c

---
+++ Array/arrayoddevensearch.c
@@ -0,0 +1,17 @@
+//Odd even search in array
+#include <stdio.h>
+int main(){
+    int i, n, j;
+    printf("Enter size of array");
+    scanf("%d", &n);                               //Enter size of array
+    int a[n];
+    printf("Enter elements in array\n");                    //Enter elements in array
+    for(i=0; i<n; i++){
+            printf("Enter the value of a[%d]=",i);
+            scanf("%d", &a[i]);
+    }
+    printf("\n");
+    printf("Array is : ");
+    for(i=0; i<n; i++){                             //Print elements of array
+            printf("Value of a[%d]=%d\n",i, a[i]);
+    }
