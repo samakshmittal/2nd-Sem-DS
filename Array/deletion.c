3ec35ef07961e7b484cb775c2962f086433808ff
samakshmittal <samakshmittal826@gmail.com>
2023-01-21 14:33:51 +0530

    Update deletion.c

--- Array/deletion.c
+++ Array/deletion.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 int main(){
-    int A[100], ch;
     int ub, j, n, loc, z=0;
     printf("Enter size of array");
     scanf("%d", &n);                               //Enter size of array
-    printf("Enter elements in array\n");
-    for (j=0; j<n; j++){                              //Entering elements in an array
-        printf("Enter element A[%d]", j);
-        scanf("%d ", &A[j]);
+    int A[n];
+    printf("Enter elements in array\n");                    //Enter elements in array
+    for(j=0; j<n; j++){
+            printf("Enter the value of a[%d]=",j);
+            scanf("%d", &A[j]);
     }
     printf("\n");
-    printf("Before deletion : ");
-    for (j=0; j<n; j++){                              //Printing elements in an array before insertion
-        printf("%d ", A[j]);
+    printf("Array is : ");
+    for(j=0; j<n; j++){                             //Print elements of array
+            printf("Value of a[%d]=%d\n",j, A[j]);
     }
     printf("\n");
-    printf("Enter location from where element is to be deleted");        //Enter location from where element is to be deleted
-    scanf("%d", &loc);                    //Enter location of element to be deleted
     ub=n-1;
     int temp=loc, i=ub;
     while(z==0)
     {
+        printf("Enter location from where element is to be deleted");        //Enter location from where element is to be deleted
+        scanf("%d", &loc);                    //Enter location of element to be deleted
         if(loc<n)
         {
             while(i>=temp){                          //Removing space
