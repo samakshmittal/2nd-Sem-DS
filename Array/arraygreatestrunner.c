80c85589c5e0e7b4b371da75bb4a5cf0262a4394
samakshmittal <samakshmittal826@gmail.com>
2023-01-26 21:22:56 +0530

    Update arraygreatestrunner.c

---
+++ Array/arraygreatestrunner.c
@@ -0,0 +1,48 @@
+//Greatest runner ups
+#include <stdio.h>
+void max(int a[], int n){
+    for(int i=0; i<n; i++){
+        int z=0, y=0;                                    //rejecting small values
+        for(int j=0; j<n; j++){
+            if(i!=j){
+                if(a[i]<a[j]){
+                    z=1;
+                }
+            }    
+        }
+        if(z==0){
+            for(int j=0; j<n; j++){
+            if(i!=j){                                //rejecting similar greatest values
+                if(a[i]==a[j]){
+                    y=2;
+                }
+            }    
+        }
+        }
+        if(y==2){
+            printf("Invalid input");
+            break;
+        }
+        else if(z==0){
+            printf("%d is greatest", a[i]);                 //printing greatest value
+        } 
+    }
+}
+int main(){
+    int i, n, j;
+    printf("Enter size of array");
+    scanf("%d", &n);                               //Enter size of array
+    int a[n];
+    printf("Enter elements in array\n");                    //Enter elements in array
+    for(i=0; i<n; i++){
+            printf("Enter the value of a[%d]=",i);
+            scanf("%d", &a[i]);
+    }
+    printf("\n");
+    printf("Array is : ");
+    for(i=0; i<n; i++){                             //Print elements of array
+            printf("Value of a[%d]=%d\n",i, a[i]);
+    }
+    max(a, n);
+    return 0;
+}
