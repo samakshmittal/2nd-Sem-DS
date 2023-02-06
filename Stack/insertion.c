7212fae469890732418579a64c62cae97bd5547a
samakshmittal <samakshmittal826@gmail.com>
2023-02-06 14:32:59 +0530

    Update insertion.c

--- Stack/insertion.c
+++ Stack/insertion.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+int top=-1;
 void enter(int stack[], int e);
 void display(int stack[]);
 int main(){
@@ -19,12 +20,17 @@ void enter(int stack[], int e){
     printf("Enter values in array");
     for(i=0; i<e; i++){
         scanf("%d", &stack[i]);
+        top++;
     }
 }
 void display(int stack[]){
-    int i=0;
-    while(stack[i]!='\0'){
-        printf("%d ", stack[i]);
-        i++;
+    int i;
+    if(top==-1){
+        printf("\nStack is empty\n");
+    }
+    else{
+        for(i=top; i>=0; i--){
+            printf("%d ", stack[i]);
+        }
     }
 }
