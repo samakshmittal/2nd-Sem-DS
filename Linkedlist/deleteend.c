6a441a143007ba2f0ba22fe4995fd2f22c1c1cec
samakshmittal <samakshmittal826@gmail.com>
2023-02-06 13:21:34 +0530

    Create deleteend.c

---
+++ Linkedlist/deleteend.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+int main(){
+    int n, a;
+    printf("Enter number of elements");
+    scanf("%d", &n);
+    struct node *p, *head, *prev, *avail, *new, *ptr;
+    for(int i=0; i<n; i++){
+        p=malloc(sizeof(struct node));
+        scanf("%d", &p->data);
+        p->next=NULL;
+        if(i==0){
+            head=p;
+            prev=p;
+        }
+        else{
+            prev->next=p;
+            prev=p;
+        }
+    }
+    if(head==NULL){
+        printf("Underflow");
+    }
+    else{
+        ptr=prev;
+        ptr->next=NULL;
+    }
+    p=head;
+    while(p!=NULL){
+            printf("%d\n", p->data);
+            p=p->next;
+        }
+    return 0;   
+}
