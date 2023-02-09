ecd39fb43415838bc17814ad568df7574d20f8fc
samakshmittal <samakshmittal826@gmail.com>
2023-02-09 10:49:06 +0530

    Update combinedinsertiondeletion1.c

--- Linkedlist/combinedinsertiondeletion1.c
+++ Linkedlist/combinedinsertiondeletion1.c
@@ -44,22 +44,29 @@ int main(){
     }while(option!=13);
 }
 struct node *create(struct node *head){
-    int n, i;
-    printf("Enter number of elements in linkedlist");
+    int n, i, a;
+    printf("Enter maximum number of elements allowed in linkedlist");
     scanf("%d", &n);
+    printf("Enter number of elements in linkedlist");
+    scanf("%d", &a);
     struct node *prev, *p;
-    for(i=0; i<n; i++){
-        p=malloc(sizeof(struct node));
-        scanf("%d", &p->data);
-        p->next=NULL;
-        if(i==0){
-            head=p;
-            prev=p;
+    if(a<n){
+        for(i=0; i<a; i++){
+            p=malloc(sizeof(struct node));
+            scanf("%d", &p->data);
+            p->next=NULL;
+            if(i==0){
+                head=p;
+                prev=p;
+            }
+            else{
+                prev->next=p;
+                prev=p;
+            }
         }
-        else{
-            prev->next=p;
-            prev=p;
-        }
+    }
+    else{
+        printf("Try again");
     }
     return head;
 }
