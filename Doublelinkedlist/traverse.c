a03f4d4a167c6d12334e38587414036291a54b09
samakshmittal <samakshmittal826@gmail.com>
2023-02-15 11:33:45 +0530

    Create traverse.c

---
+++ Doublelinkedlist/traverse.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+struct node{
+    struct node *prev;
+    int data;
+    struct node *next;    
+};
+int main(){
+    int n, a;
+    printf("Enter number of elements");
+    scanf("%d", &n);
+    struct node *p, *head, *prev1, *new, *s;
+    for(int i=0; i<n; i++){
+        p=malloc(sizeof(struct node));
+        scanf("%d", &p->data);
+        p->next=NULL;
+        if(i==0){
+            head=p;
+            prev1=p;
+            s=p;
+            head->prev=NULL;
+        }
+        else{
+            prev1->next=p;
+            s=prev1;
+            prev1->prev=s;
+            prev1=p;
+        }
+    }
+    p=head;
+    while(p!=NULL){
+        printf("%d\n", p->prev);
+        printf("%d\n", p->data);
+        printf("%d\n", p->next);
+        p=p->next;
+    }
+}
