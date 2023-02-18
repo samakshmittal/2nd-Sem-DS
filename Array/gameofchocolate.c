4a644de09132a409ae0d9ac397e4124702aef3ea
samakshmittal <samakshmittal826@gmail.com>
2023-02-18 22:44:34 +0530

    Update gameofchocolate.c

--- Array/gameofchocolate.c
+++ Array/gameofchocolate.c
@@ -2,7 +2,7 @@
 int main(){
     int t, n, i, j, x;
     scanf("%d", &t);          //no of games
-    for(i=1; i<=n; i++){
+    for(i=1; i<=t; i++){
         scanf("%d", &n);
         int arr[n];
         for(j=0; j<n; j++){
@@ -10,11 +10,10 @@ int main(){
         }
         x=arr[0];
         for(j=0; j<n; j++){
-            if(arr[j]>x){
+            if(arr[j]<x){
                 x=arr[j];
             }
         }
-        printf("%d", x);
         printf("%d\n", x*(n-1));
     }
 }
