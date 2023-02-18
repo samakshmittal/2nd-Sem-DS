941597b6b8022583d05674b40ba7e71a06c5c2b7
samakshmittal <samakshmittal826@gmail.com>
2023-02-18 22:37:07 +0530

    Create gameofchocolate.c

---
+++ Array/gameofchocolate.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int t, n, i, j, x;
+    scanf("%d", &t);          //no of games
+    for(i=1; i<=n; i++){
+        scanf("%d", &n);
+        int arr[n];
+        for(j=0; j<n; j++){
+            scanf("%d", &arr[i]);
+        }
+        x=arr[0];
+        for(j=0; j<n; j++){
+            if(arr[j]>x){
+                x=arr[j];
+            }
+        }
+        printf("%d", x);
+        printf("%d\n", x*(n-1));
+    }
+}
