7cc4c6cc063d4895ea4a7acebfd5c68555ad8088
samakshmittal <samakshmittal826@gmail.com>
2023-03-27 21:28:15 +0530

    Create insertion.c

---
+++ Queue/Array/insertion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+    int n, front=-1, rear=-1, ch, i;
+    printf("Enter max size of array");
+    scanf("%d", &n);
+    int q[n];
+    printf("Enter 0 to insert element");
+    scanf("%d", &ch);
+    while(ch==0){
+        if(rear==n-1){
+            printf("Overflow");
+        }
+        else{
+            if(front==-1){
+                rear++;
+                printf("Enter data");
+                scanf("%d", &q[rear]);
+                front=rear;
+            }
+            else{
+                rear++;
+                printf("Enter data");
+                scanf("%d", &q[rear]);
+            }
+        }
+        printf("Enter 0 to insert element");
+        scanf("%d", &ch);
+    }
+    for(i=0; i<=rear; i++){
+        printf("%d ", q[i]);
+    }
+    return 0;
+}
