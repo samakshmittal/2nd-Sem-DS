3cd81c8eece4f1c6b749d054ece1e6ae2db7e444
samakshmittal <samakshmittal826@gmail.com>
2023-03-31 23:12:44 +0530

    Update tempCodeRunnerFile.c

--- Stack/Applications/PolishNotation/tempCodeRunnerFile.c
+++ Stack/Applications/PolishNotation/tempCodeRunnerFile.c
@@ -1,1 +1,9 @@
-isdigit(source[i]) || isalpha(source[i])
+int x=strlen(source);
+    // source[x]=')';
+    // x++;
+    // int ub=x-1;
+    // while(ub>=i){
+    //     source[ub+1]=source[ub];
+    //     ub--;
+    // }
+    // source[i]='(';
