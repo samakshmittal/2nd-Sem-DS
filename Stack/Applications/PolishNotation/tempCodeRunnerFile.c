726d4bc2d43daf0a2d4f32abb55cac56930b5bc1
samakshmittal <samakshmittal826@gmail.com>
2023-03-31 23:26:12 +0530

    Update tempCodeRunnerFile.c

--- Stack/Applications/PolishNotation/tempCodeRunnerFile.c
+++ Stack/Applications/PolishNotation/tempCodeRunnerFile.c
@@ -1,9 +1,9 @@
-int x=strlen(source);
-    // source[x]=')';
-    // x++;
-    // int ub=x-1;
-    // while(ub>=i){
-    //     source[ub+1]=source[ub];
-    //     ub--;
-    // }
-    // source[i]='(';
+   int x=strlen(source);
+    source[x]=')';
+    x++;
+    int ub=x-1;
+    while(ub>=0){
+        source[ub+1]=source[ub];
+        ub--;
+    }
+    source[0]='(';
