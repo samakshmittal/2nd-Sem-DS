92853133bfa2e49334bbcf00472b3251e3489e39
samakshmittal <samakshmittal826@gmail.com>
2023-04-01 08:58:52 +0530

    Update projectqueue.c

--- Queue/Array/projectqueue.c
+++ Queue/Array/projectqueue.c
@@ -1,58 +1,70 @@
 #include <stdio.h>
-void insert(int arr[], int n);
-void display(int arr[]);
-int front=-1, rear=-1, n, i;
+void insert();
+void display();
+void delete();
+int front=-1, rear=-1, i;
+int queue[10];
 int main(){
     int e, ch;
-    printf("Enter size of array");
-    scanf("%d", &n);
-    int queue[n];
     do{
         printf("\nWhat do you want to do:\n1 for insert\n2 for delete\n3 for display\n4 for exit");
         scanf("%d", &ch);
         switch(ch){
             case 1:
-            insert(queue, n);
+            insert();
             break;
-            /*case 2:
-            value=pop(stack);
-            if(value!=-1){
-                printf("%d\n", value);
-            }
-            else if(value==-1){
-                if(top!=-1){
-                    printf("%d", value);
-                }
-            }
-            break;*/
+            case 2:
+            delete();
+            break;
             case 3:
-            display(queue);
+            display();
             break;
         }
     }while(ch!=4);
     return 0;
 }
-void insert(int arr[], int n){
-    int front=-1, rear=-1;
-    if(rear==n-1){
+void insert(){
+    if(rear==9){
         printf("Overflow");
     }
     else{
         if(front==-1){
             rear++;
             printf("Enter data");
-            scanf("%d", &arr[rear]);
+            scanf("%d", &queue[rear]);
             front=rear;
         }
         else{
             rear++;
             printf("Enter data");
-            scanf("%d", &arr[rear]);
+            scanf("%d", &queue[rear]);
         }
     }
 }
-void display(int arr[]){
-    for(i=0; i<=rear; i++){
-        printf("%d ", arr[i]);
+void display(){
+    if(front!=-1){
+        for(i=front; i<=rear; i++){
+            printf("%d ", queue[i]);
+        }
+    }
+    else{
+        printf("Queue is empty");
+    }
+}
+void delete(){
+    int data;
+    if(front==-1){
+        printf("Underflow");
+    }
+    else{
+        if(front==rear){
+            data=queue[front];
+            front=-1;
+            rear=-1;
+        }
+        else{
+            data=queue[front];
+            front++;
+        }
     }
 }
