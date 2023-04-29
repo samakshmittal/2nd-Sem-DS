9cff9502c54590b977f7ee28f867ce2286cc7c96
samakshmittal <samakshmittal826@gmail.com>
2023-04-29 19:49:21 +0530

    Create postorder.c

---
+++ Tree/Binary_Search_Tree/postorder.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node *root=NULL, *ptr, *ptr1, *preptr;
+void chec(struct node *ptr1, struct node *ptr){
+    while(ptr1!=NULL){
+        if(ptr1->data>ptr->data){
+            preptr=ptr1;
+            ptr1=ptr1->left;
+            chec(ptr1, ptr);
+        }
+        else if(ptr1->data<ptr->data){
+            preptr=ptr1;
+            ptr1=ptr1->right;
+            chec(ptr1, ptr);
+        }
+    }
+}
+void postorder(struct node *ptr){
+    if(ptr!=NULL){
+        postorder(ptr->left);
+        postorder(ptr->right);
+        printf("%d ", ptr->data);
+    }
+}
+int main(){
+    int ch;
+    printf("Enter -1 to stop");
+    scanf("%d", &ch);
+    while(ch!=-1){
+        ptr=malloc(sizeof(struct node));
+        printf("Enter data");
+        scanf("%d", &ptr->data);
+        ptr->left=NULL;
+        ptr->right=NULL;
+        if(root==NULL){
+            root=ptr;
+        }
+        else{
+            ptr1=root;
+            chec(ptr1, ptr);
+            if(preptr->data>ptr->data){
+                preptr->left=ptr;
+            }
+            else if(preptr->data<ptr->data){
+                preptr->right=ptr;
+            }
+        }
+        printf("Enter -1 to stop");
+        scanf("%d", &ch);
+    }
+    //case 3:
+    ptr=root;
+    postorder(ptr);
+    return 0;
+}
