0d131e169636e763f87d8b7f059c026d3c35fd01
Samaksh Mittal <samakshmittal826@gmail.com>
2023-11-11 17:02:49 +0530

    Update bubble_sorting.c

--- Sorting/bubble_sorting.c
+++ Sorting/bubble_sorting.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
+#include <stdio.h>                               // Time Complexity
 int main(){
-    int n, i, j;
-    printf("Enter max size of array");
-    scanf("%d", &n);
-    int arr[n];
-    for(i=0; i<n; i++){
-        printf("Enter data");
-        scanf("%d", &arr[i]);
+    int n, i, j;                                 // Constant
+    printf("Enter max size of array");           // Constant
+    scanf("%d", &n);                             // Constant
+    int arr[n];                                  // Constant
+    for(i=0; i<n; i++){                          // n+1 times                       entering data
+        printf("Enter data");                    // n times                         print statement             
+        scanf("%d", &arr[i]);                    // n times                         scan statement
     }
-    for (i=0; i<n; i++){
-        for(j=0; j<n; j++){
-            if(arr[j]>arr[j+1]){
-                int t=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=t;
+    for (i=0; i<n-1; i++){                       // n times                         outer loop iteration for sorting or number of passes
+        for(j=0; j<n-1-i; j++){                  // n(n+1)/2 times                  outer loop iteration for sorting or number of bubbles made in each sort
+            if(arr[j]>arr[j+1]){                 // n(n+1)/2 - 1 times              number of comparisons made
+                int t=arr[j];                    // Constant
+                arr[j]=arr[j+1];                 // Constant
+                arr[j+1]=t;                      // Constant
             }
         }
     }
-    for(i=0; i<n; i++){
-        printf("%d ", arr[i]);
+    for(i=0; i<n; i++){                          // n+1 times                       printing sorted array
+        printf("%d ", arr[i]);                   // n times                         print statement 
     }
-    return 0;
+    return 0;                                    // Constant
 }
